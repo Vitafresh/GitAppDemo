8dca945306decd7acf0ec2a3f9b186112c9fd67e
Vitafresh <vvitafresh@gmail.com>
2019-01-11 14:15:58 +0200

    Initial commit

---
+++ app/src/main/java/top/vitafresh/gitappdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package top.vitafresh.gitappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//First test comment
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
